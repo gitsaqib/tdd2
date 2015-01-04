9370aa5f7559bb861eef6036d374507259df1800
Saqib Butt <saqib.butt@globalnorthstar.com>
2015-01-04 17:57:40 +0500

    Chapter 19.

--- src/tdd2/WasRunTest.java
+++ src/tdd2/WasRunTest.java
@@ -13,5 +13,26 @@ public class WasRunTest {
 		wasRun.run();
 		assertTrue(wasRun.wasRun());
 	}
-
+	@Test
+	public void testWasUpCalled() {
+		WasRun wasRun=new WasRun("testMethod");
+		assertFalse(wasRun.wasRun());
+		wasRun.run();
+		assertTrue(wasRun.wasRun());
+		
+	}
+	@Test
+	public void testSetup() {
+		WasRun test = TestCase.setUp();
+		test.run();
+//		assertTrue(test.wasSetUp());
+	}
+//		def setUp(self):
+//		self.test= WasRun("testMethod")
+//		def testRunning(self):
+//		self.test.run()
+//		assert(self.test.wasRun)
+//		def testSetUp(self):
+//		self.test.run()
+//		assert(self.test.wasSetUp)
 }
